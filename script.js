2bb72968b2b867ea8ed6307cac9a6345b4761e4d
vasenkom <m.vasenko@yandex.ru>
2024-04-19 22:50:14 +0200

    add function addBookToLibrary() that is supposed to display users input

--- script.js
+++ script.js
@@ -8,5 +8,21 @@ function Book() {
 }
 
 function addBookToLibrary() {
-    
+    const cardJS = document.createElement("div");
+    cardJS.classList.add("card");
+
+    const imgJS = document.createElement("img");
+    imgJS.classList.add("imgBook");
+    imgJS.src = imgInputSRC;
+    cardJS.appendChild(imgJS);
+
+    const nameJS = document.createElement("p");
+    nameJS.textContent = nameInputText;
+    nameJS.classList.add("name");
+    cardJS.appendChild(nameJS);
+
+    const authorJS = document.createElement("p");
+    authorJS.textContent = authorInputText;
+    authorJS.classList.add("author");
+    cardJS.appendChild(authorJS);
 }
