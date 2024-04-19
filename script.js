e798fa6ab0e59532232fc5d19a3f99edcc432451
vasenkom <m.vasenko@yandex.ru>
2024-04-19 21:28:08 +0200

    add book() function

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book() {
+    this.author = author;
+    this.bookName = bookName;
+    this.pagesNumber = pagesNumber;
+    this.readStatus = this.readStatus; //if the book was read by user or no
+}
+
+function addBookToLibrary() {
+    
+}
